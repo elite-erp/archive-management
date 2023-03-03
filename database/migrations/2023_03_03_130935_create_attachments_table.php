b2c499992e457a040761947ed57fe41e92bdd677
gallab <mohanad.gallab.gabir@gmail.com>
2023-03-03 15:59:38 +0200

    finish migrate , start controller and model

---
+++ database/migrations/2023_03_03_130935_create_attachments_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attachments', function (Blueprint $table) {
+            $table->id();
+            $table->string('file');
+            $table->timestamps();
+        });
+
+        // Pevot Table
+
+        Schema::create('attachment_document', function (Blueprint $table) {
+
+            $table->integer('document_id')->unsigned();
+        
+            $table->integer('attachment_id')->unsigned();
+        
+            $table->foreign('document_id')->references('id')->on('documents')
+        
+                ->onDelete('cascade');
+        
+            $table->foreign('attachment_id')->references('id')->on('attachments')
+        
+                ->onDelete('cascade');
+        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attachments');
+        Schema::dropIfExists('attachment_document');
+    }
+};
