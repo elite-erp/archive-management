b59c17f626129187056c02d216cc365c99fd5a2c
alnoor altegany <nour.dev.00@gmail.com>
2023-03-12 15:36:02 +0200

    move route definitions from api to web . install frontend dependances with pnpm

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\AttachmentController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DocumentController;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +21,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/authenticate', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::resource('documents', DocumentController::class );
+Route::resource('categories', CategoryController::class );
+Route::resource('attachments', AttachmentController::class);
