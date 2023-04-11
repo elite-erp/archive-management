349f66b7a145348f0e4eb21b4ed9f7f13a358b9d
alnoor altegany <nour.dev.00@gmail.com>
2023-04-12 00:45:19 +0200

    feat(api): add auth middlware for all protected routes as group

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CPanelController;
 use App\Http\Controllers\DocumentController;
 use App\Http\Controllers\UsersController;
-use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,16 +19,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', CPanelController::class)->name('cpanel')->middleware('auth');
-Route::get('/profile', [AuthController::class, 'profile']);
-Route::resource('/users', UsersController::class);
-
 Route::get('/login', [AuthController::class, 'login'])->middleware('guest')->name('login');
 Route::post('/authenticate', [AuthController::class, 'authenticate']);
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
-Route::resource('documents', DocumentController::class );
-Route::resource('categories', CategoryController::class );
-Route::resource('attachments', AttachmentController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/', CPanelController::class)->name('cpanel');
+    Route::get('/profile', [AuthController::class, 'profile']);
+    Route::post('/users/{id}', [UsersController::class, 'uploadPhoto']);
+    Route::resource('/users', UsersController::class);
+
+
+    Route::resource('documents', DocumentController::class);
+    Route::resource('categories', CategoryController::class);
+    Route::resource('attachments', AttachmentController::class);
+});
