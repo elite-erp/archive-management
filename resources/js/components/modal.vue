e186f4de2c0878402599ec86c737e399828e18fd
alnoor altegany <nour.dev.00@gmail.com>
2023-04-11 22:14:38 +0200

    feat(ui): make modal fixed. hide scroll toggle scroll from body when show modal

--- resources/js/components/modal.vue
+++ resources/js/components/modal.vue
@@ -1,14 +1,22 @@
 <template>
   <section
-    class="absolute flex flex-row justify-center items-center top-0 left-0 bg-gray-500/50 h-screen w-screen"
+    class="fixed flex flex-row justify-center items-center top-0 left-0 bg-gray-500/50 min-h-screen h-full w-screen overflow-y-hidden"
     @click.self="emits('close')"
   >
-    <ECard class="w-6/12 h-[94%] flex flex-col">
+    <ECard class="w-6/12 max-h-[94%] flex flex-col">
       <slot />
     </ECard>
   </section>
 </template>
 
 <script setup>
+import { onMounted, onUnmounted } from "vue";
+
 const emits = defineEmits(["close"]);
+onMounted(() => {
+  document.body.classList = "overflow-y-clip";
+});
+onUnmounted(() => {
+  document.body.classList = "";
+});
 </script>
