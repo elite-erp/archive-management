ed47afdfe85e66f111d96763d0c8de10b3d84826
alnoor altegany <nour.dev.00@gmail.com>
2023-04-11 22:02:57 +0200

    fix(db): default absolute path to user photo

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->string('photo')->default('imgs/user.png');
+            $table->string('photo')->default('/imgs/user.png');
             $table->string('password');
             $table->string('role')->default('موظف');
             $table->rememberToken();
