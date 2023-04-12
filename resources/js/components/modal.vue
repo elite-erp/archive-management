97295d26ef1b46cc30a3e195bdf00098e42b3446
alnoor altegany <nour.dev.00@gmail.com>
2023-04-12 11:57:02 +0200

    refactor(ui): handle teleport in modal component

--- resources/js/components/modal.vue
+++ resources/js/components/modal.vue
@@ -1,16 +1,18 @@
 <template>
-  <section
-    class="absolute flex flex-row justify-center items-center top-0 left-0 bg-gray-500/50 min-h-screen h-full w-screen overflow-y-hidden"
-    @click.self="emits('close')"
-  >
-    <ECard class="w-6/12 min-h-fit max-h-[94%] flex flex-col pt-2">
-      <slot />
-    </ECard>
-  </section>
+  <Teleport to="body">
+    <section
+      class="absolute flex flex-row justify-center items-center top-0 left-0 bg-gray-500/50 min-h-screen h-full w-screen overflow-y-hidden"
+      @click.self="emits('close')"
+    >
+      <ECard class="w-6/12 min-h-fit max-h-[94%] flex flex-col pt-2">
+        <slot />
+      </ECard>
+    </section>
+  </Teleport>
 </template>
 
 <script setup>
-import { onMounted, onUnmounted } from "vue";
+import { onMounted, onUnmounted, Teleport } from "vue";
 
 const emits = defineEmits(["close"]);
 onMounted(() => {
