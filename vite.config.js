8c5cb02e988597af4b1ae501af3b7508b648c2d4
alnoor altegany <nour.dev.00@gmail.com>
2023-04-12 21:21:32 +0200

    test(ui): add typechecker for vite, vue. setup tsconfig. use vue-tsc

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
+import checker from "vite-plugin-checker";
 
 export default defineConfig({
   plugins: [
+    checker({
+      vueTsc: true,
+      typescript: true,
+    }),
     vue(),
     laravel({
       input: ["resources/css/app.css", "resources/js/app.js"],
