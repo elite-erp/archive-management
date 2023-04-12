f354b498159754bcb7bcf527052a9ab9d32d16a5
alnoor altegany <nour.dev.00@gmail.com>
2023-04-12 11:56:00 +0200

    feat(ui): extract pagination to component

---
+++ resources/js/components/pagination.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="pb-10 text-gray-500 flex gap-4 justify-end mx-[90px]">
+    <Link
+      class="h-10 w-10 inline-flex justify-center items-center btn-icon hover:text-primary-500 hover:font-bold"
+      :href="list.links[0].url"
+    >
+      <EIcon :name="mdiArrowRight" />
+    </Link>
+    <Link
+      class="h-10 w-10 inline-flex justify-center items-center btn-icon hover:text-primary-500 hover:font-bold"
+      :href="link.url"
+      v-for="(link, index) in filterLinks(list.links)"
+      :key="index"
+      v-html="link.label"
+    />
+    <Link
+      class="h-10 w-10 inline-flex justify-center items-center btn-icon hover:text-primary-500 hover:font-bold"
+      :href="list.links[list.links.length - 1].url"
+    >
+      <EIcon :name="mdiArrowLeft" />
+    </Link>
+  </div>
+</template>
+<script lang="ts" setup>
+import { mdiArrowRight, mdiArrowLeft } from "@mdi/js";
+import { filterLinks } from "../utils";
+
+defineProps(["list"]);
+</script>
