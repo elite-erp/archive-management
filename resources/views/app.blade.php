ede4d8b626e02f493885b6e5b0f43fb4a34264ea
alnoor altegany <nour.dev.00@gmail.com>
2023-04-12 02:12:33 +0200

    chore: add icon and text

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Elite Archive</title>
+    <link rel="icon" href="/assets/icons/logo.png" />
+    <title>إلايت أرشيف</title>
 
     @vite('resources/js/app.js')
     @inertiaHead
