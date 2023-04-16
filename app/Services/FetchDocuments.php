7bb74edaca9cae91788f33d9855a8595b1721938
alnoor altegany <nour.dev.00@gmail.com>
2023-04-16 15:29:29 +0200

    feat(ui, documents): handle pagination with search

--- app/Services/FetchDocuments.php
+++ app/Services/FetchDocuments.php
@@ -4,9 +4,10 @@ use App\Models\Document;
 
 function fetchDocuments($search, $id = null, $field = null)
 {
-    return Document::when($id, function($query) use ($id, $field) {
+    return Document::when($id, function ($query) use ($id, $field) {
         $query->where($field, $id);
     })->where(function ($query) use ($search) {
-        $query->where('title', 'like', "%{$search}%");
-    })->with('category')->latest()->paginate(10);
+        $query->where('title', 'like', "%{$search}%")
+            ->orWhere('description', 'like', "%{$search}%");
+    })->with('category')->latest()->paginate(10)->withQueryString();
 }
